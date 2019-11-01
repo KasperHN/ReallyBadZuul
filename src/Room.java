2b65007ef971dec0e484e8e6caf3769c2762638c
KasperHN <kasper_130195@live.dk>
2019-11-01 09:11:26 +0100

    idek

--- src/Room.java
+++ src/Room.java
@@ -1,19 +1,21 @@
+
 /**
  * Class Room - a room in an adventure game.
  *
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
+ * This class is part of the "World of Zuul" application.
+ * "World of Zuul" is a very simple, text based adventure game.
  *
- * A "Room" represents one location in the scenery of the game.  It is 
- * connected to other rooms via exits.  The exits are labelled north, 
+ * A "Room" represents one location in the scenery of the game.  It is
+ * connected to other rooms via exits.  The exits are labelled north,
  * east, south, west.  For each direction, the room stores a reference
  * to the neighboring room, or null if there is no exit in that direction.
- * 
+ *
  * @author  Michael Kölling and David J. Barnes
  * @version 2011.07.31
  */
-public class Room 
+public class Room
 {
+
     public String description;
     public Room northExit;
     public Room southExit;
@@ -21,34 +23,64 @@ public class Room
     public Room westExit;
 
     /**
-     * Create a room described "description". Initially, it has
-     * no exits. "description" is something like "a kitchen" or
-     * "an open court yard".
+     * Create a room described "description". Initially, it has no exits.
+     * "description" is something like "a kitchen" or "an open court yard".
+     *
      * @param description The room's description.
      */
-    public Room(String description) 
+    public Room(String description)
     {
         this.description = description;
     }
 
     /**
-     * Define the exits of this room.  Every direction either leads
-     * to another room or is null (no exit there).
+     * Define the exits of this room. Every direction either leads to another
+     * room or is null (no exit there).
+     *
      * @param north The north exit.
      * @param east The east east.
      * @param south The south exit.
      * @param west The west exit.
      */
-    public void setExits(Room north, Room east, Room south, Room west) 
+    public void setExits(Room north, Room east, Room south, Room west)
     {
-        if(north != null)
+        if (north != null)
+        {
             northExit = north;
-        if(east != null)
+        }
+        if (east != null)
+        {
             eastExit = east;
-        if(south != null)
+        }
+        if (south != null)
+        {
             southExit = south;
-        if(west != null)
+        }
+        if (west != null)
+        {
             westExit = west;
+        }
+    }
+
+    public Room getExit(String direction)
+    {
+        if (direction.equals("north"))
+        {
+            return northExit;
+        }
+        if (direction.equals("east"))
+        {
+            return eastExit;
+        }
+        if (direction.equals("south"))
+        {
+            return southExit;
+        }
+        if (direction.equals("west"))
+        {
+            return westExit;
+        }
+        return null;
     }
 
     /**
